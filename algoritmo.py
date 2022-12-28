39a3c299059e557e744898d7018b413e9e466c2f
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-12-28 15:26:53 -0300

    Initial commit

---
+++ algoritmo.py
@@ -0,0 +1,143 @@
+import cv2 as cv
+from numpy import full, uint8
+from itertools import product
+from os.path import isfile
+from os import system
+
+BLACK, WHITE, RED = 0, 255, (0, 0, 255)
+
+def bgr2gray(entrie: cv.Mat):
+  return cv.cvtColor(entrie, cv.COLOR_BGR2GRAY)
+
+def gray2bgr(entrie: cv.Mat):
+  return cv.cvtColor(entrie, cv.COLOR_GRAY2BGR)
+
+def openImage(entrie: str, convert = True):
+  if not isfile(entrie):
+    print(f"\"{entrie}\" inexistente!"); exit(0)
+  temp = cv.imread(entrie)
+  return bgr2gray(temp) if convert else temp
+
+def saveImage(fname: str, entrie: cv.Mat, convert = True):
+  temp = gray2bgr(entrie) if convert else entrie
+  return cv.imwrite(fname, temp)
+
+def getTuplas(entrie: cv.Mat.shape):
+  return product(range(entrie[0]), range(entrie[1]))
+
+def claheFilter(entrie: cv.Mat):
+  a, b, c = cv.split(gray2bgr(entrie))
+  a = cv.createCLAHE(15, (8, 8)).apply(a)
+  return bgr2gray(cv.merge((a, b, c)))
+
+def bilateral(entrie: cv.Mat, key = 7) -> cv.Mat:
+  return cv.bilateralFilter(entrie, key, 75, 75)
+
+def cannyFilter(entrie: cv.Mat) -> cv.Mat:
+  return cv.Canny(entrie, 50, 250, L2gradient = True)
+
+def xFilter(entrie: cv.Mat, length = 10):
+  output = entrie.copy()
+  def xFilter(pair1: int, pair2: int):
+    def xFilter(cord1: int, cord2: int):
+      pixel1 = output[cord1, pair2]
+      pixel2 = output[cord2, pair2]
+      if pixel1 == pixel2 == WHITE:
+        for x in range(cord1, cord2 + 1):
+          output[x, pair2] = BLACK
+        output[pair1, pair2] = WHITE
+        return True
+      return False
+    cord1 = pair1 - length
+    cord2 = pair1 + length
+    while cord1 != cord2:
+      if xFilter(cord1, cord2): break
+      cord1 += 1
+      if xFilter(cord1, cord2): break
+      cord1 -= 1; cord2 -= 1
+      if xFilter(cord1, cord2): break
+      cord1 += 1
+  altura, largura = output.shape
+  atr1, atr2 = length + 1, altura - length
+  tuplas = product(range(atr1, atr2), range(largura))
+  for x, y in tuplas: xFilter(x, y)
+  return output
+
+def organizeData(entrie: list, dist = 10, tam = 30):
+  templist1 = []
+  for sublista in entrie:
+    temp = [sublista[0]]
+    for tupla in sublista[1:]:
+      if tupla[1] == temp[-1][1]:
+        del temp[-1]
+      temp.append(tupla)
+    templist1.append(temp)
+  templist2 = []
+  for sublista in templist1:
+    temp = [sublista[0]]
+    for tupla in sublista[1:]:
+      y1 = tupla[1]
+      y2 = temp[-1][1]
+      if (y1 - y2) > dist and len(temp) < tam:
+        temp.clear()
+      temp.append(tupla)
+    templist2.append(temp)
+  return templist2
+
+def extract(entrie: cv.Mat, fname: str, radius = 5):
+  tuplas = getTuplas(entrie.shape)
+  tuplas = [x for x in tuplas if entrie[x] == WHITE]
+  tuplas = sorted(tuplas)
+  with open(f"{fname}.tup.txt", "w") as saida:
+    for tupla in tuplas: saida.write(f"{tupla}\n")
+  listas: list[list[tuple[int, int]]] = []
+  pos1 = 0; lentup = len(tuplas)
+  while pos1 != lentup:
+    temp = [tuplas[pos1]]
+    pos1 += 1; pos2 = lentup
+    for i in range(pos1, lentup):
+      x1 = tuplas[i][0]
+      x2 = temp[-1][0]
+      if (x1 - x2) > radius:
+        pos2 = i; break
+      temp.append(tuplas[i])
+    listas.append(temp)
+    pos1 = pos2
+  listas.sort(key = lambda x: len(x), reverse = True)
+  for elem in listas: elem.sort(key = lambda x: x[1])
+  listas = organizeData(listas)
+  preta = full(entrie.shape, BLACK, uint8)
+  sobre = gray2bgr(entrie)
+  for i in range(3):
+    imagem = preta.copy()
+    with open(f"{fname}{i}.txt", "w") as saida:
+      for tupla in listas[i]:
+        saida.write(f"{tupla}\n")
+        imagem[tupla] = WHITE
+        sobre[tupla] = RED
+    saveImage(f"{fname}{i}.png", imagem)
+  return sobre
+
+def sobrepor(canny: cv.Mat, aux: cv.Mat):
+  convert = gray2bgr(canny)
+  for tupla in getTuplas(aux.shape):
+    if tuple(aux[tupla]) == RED:
+      convert[tupla] = RED
+  return convert
+
+if __name__ == "__main__":
+  system("cls||clear")
+  entrie = input("Entrie archive: ")
+  opened = openImage(entrie)
+  name = entrie[:entrie.index(".")]
+  output = claheFilter(opened)
+  saveImage(f"{name}.clahe.png", output)
+  output = bilateral(output)
+  saveImage(f"{name}.bilat.png", output)
+  canny = cannyFilter(output)
+  saveImage(f"{name}.canny.png", canny)
+  output = xFilter(canny)
+  saveImage(f"{name}.filter.png", output)
+  saida = extract(output, name)
+  output = sobrepor(canny, saida)
+  saveImage(f"{name}.sobre.png", output, 0)
---
+++ teste.py
@@ -0,0 +1,174 @@
+from PIL import Image, ImageFilter
+import cv2 as cv
+import numpy as np
+from math import sqrt
+from itertools import product
+
+BLACK, WHITE = 0, 255
+
+def modaPixel(entrie: Image.Image):
+  dictModa = {}
+  def modaPixel(entrie: tuple):
+    if entrie in dictModa:
+      dictModa[entrie] += 1
+      return
+    dictModa[entrie] = 1
+  for x in range(entrie.size[0]):
+    for y in range(entrie.size[1]):
+      modaPixel(entrie.getpixel((x, y)))
+  return dictModa
+
+def writeDict(entrie: dict, output: str):
+  with open(output, "w") as saida:
+    for key, value in entrie.items():
+      saida.write(f"{key} - {value}\n")
+
+def xFilter(entrie: Image.Image, length: int):
+  def xFilter(entrie: list):
+    resul = False
+    for i in range(1, length - 1):
+      resul = (resul or bool(entrie[i]))
+    return (not bool(entrie[0]) and resul and not bool(entrie[-1]))
+  output = entrie.copy()
+  lista = []
+  for x in range(entrie.size[0] - length + 1):
+    for y in range(entrie.size[1]):
+      for i in range(length):
+        lista.append(entrie.getpixel((x + i, y)))
+      if xFilter(lista):
+        for i in range(1, length - 1):
+          output.putpixel((x + i, y), WHITE)
+      lista.clear()
+  return output
+
+def extract(entrie: cv.Mat, radius = 5):
+  convert = rgb2gray(entrie)
+  def extract(entrie: tuple[int, int]):
+    def extract(entrie: tuple[int, int]):
+      for x, y in product(range(-radius, radius + 1), range(1, radius + 1)):
+        tupla = entrie[0] + x, entrie[1] + y
+        if convert[tupla] == BLACK: return tupla
+    entrada, lista = entrie, []
+    lista.append(entrada)
+    while True:
+      entrada = extract(entrada)
+      if entrada is None: break
+      lista.append(entrada)
+    return lista
+  lista = []
+  for tupla in product(range(convert.shape[0]), range(convert.shape[1] // 5)):
+    if convert[tupla] == BLACK: lista.append(extract(tupla))
+  with open("arquivo.txt", "w") as saida:
+    for elem in sorted(lista): saida.write(f"{elem}\n")
+
+def yFilterNoises(entrie: Image.Image, length: int):
+  def yFilter(entrie: list):
+    resul = False
+    for i in range(1, length - 1):
+      resul = (resul or bool(entrie[i]))
+    return (not bool(entrie[0]) and resul and not bool(entrie[-1]))
+  output = entrie.copy()
+  lista = []
+  for y in range(entrie.size[1] - length + 1):
+    for x in range(entrie.size[0]):
+      for i in range(length):
+        lista.append(entrie.getpixel((x, y + i)))
+      if yFilter(lista):
+        for i in range(1, length - 1):
+          output.putpixel((x, y + i), WHITE)
+      lista.clear()
+  return output
+
+def sobelFilter(entrie: Image.Image):
+  XSOBEL = ImageFilter.Kernel((3, 3), [-1, 0, 1, -2, 0, 2, -1, 0, 1], 1, 0)
+  YSOBEL = ImageFilter.Kernel((3, 3), [-1, -2, -1, 0, 0, 0, 1, 2, 1], 1, 0)
+  entrie = entrie.convert("L")
+  xsobel, ysobel = entrie.filter(XSOBEL), entrie.filter(YSOBEL)
+  output = Image.new("L", entrie.size)
+  for x in range(entrie.size[0]):
+    for y in range(entrie.size[1]):
+      xpixel = xsobel.getpixel((x, y))
+      ypixel = ysobel.getpixel((x, y))
+      value = int(sqrt(xpixel**2 + ypixel**2))
+      output.putpixel((x, y), min(value, 255))
+  return output
+
+def binarize(entrie: Image.Image, llevel = 80):
+  def binarize(entrie: int):
+    if entrie < llevel: return WHITE
+    return BLACK
+  output = Image.new("L", entrie.size)
+  for x in range(entrie.size[0]):
+    for y in range(entrie.size[1]):
+      pixel = entrie.getpixel((x, y))
+      pixel = binarize(pixel)
+      output.putpixel((x, y), pixel)
+  return output
+
+def r1Filter(entrie: Image.Image):
+  CHECK1 = [5, 6, 7, 8, 9, 12]
+  CHECK2 = [10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+  def r1Filter(entrie: list):
+    res1, res2 = True, False
+    for i in range(25):
+      if i in CHECK1: res1 = (res1 and bool(entrie[i] == BLACK))
+      elif i in CHECK2: res2 = (res2 or bool(entrie[i] == WHITE))
+    return (res1 and res2)
+  output = entrie.copy()
+  lista = []
+  for x in range(entrie.size[0] - 4):
+    for y in range(entrie.size[1] - 4):
+      for i in range(5):
+        for j in range(5):
+          lista.append(entrie.getpixel((x + i, y + j)))
+      if r1Filter(lista):
+        output.putpixel((x + 2, y + 2), WHITE)
+      lista.clear()
+  return output
+
+def r2Filter(entrie: Image.Image):
+  CHECK1 = [3, 7, 8, 13, 17, 18, 23]
+  CHECK2 = [1, 6, 11, 16, 21]
+  def r2Filter(entrie: list):
+    res1, res2 = True, False
+    for i in range(25):
+      if i in CHECK1: res1 = (res1 and bool(entrie[i] == BLACK))
+      elif i in CHECK2: res2 = (res2 or bool(entrie[i] == WHITE))
+    return (res1 and res2)
+  output = entrie.copy()
+  lista = []
+  for x in range(entrie.size[0] - 4):
+    for y in range(entrie.size[1] - 4):
+      for i in range(5):
+        for j in range(5):
+          lista.append(entrie.getpixel((x + i, y + j)))
+      if r2Filter(lista):
+        output.putpixel((x + 2, y + 2), WHITE)
+      lista.clear()
+  return output
+
+def fillContour(entrie: cv.Mat):
+  thresh = cv.threshold(entrie, 128, 255, cv.THRESH_BINARY)[1]
+  KERNEL = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+  thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, KERNEL)
+  return cv.cvtColor(np.zeros_like(thresh), cv.COLOR_RGB2BGR)
+
+def boxBlur(entrie: Image.Image, radius = 3):
+  FILTER = ImageFilter.BoxBlur(radius)
+  return entrie.filter(FILTER)
+
+def sobelFilter(entrie: cv.Mat) -> cv.Mat:
+  xsobel = cv.Sobel(entrie, cv.CV_16S, 1, 0, ksize = 3, scale = 1)
+  ysobel = cv.Sobel(entrie, cv.CV_16S, 0, 1, ksize = 3, scale = 1)
+  xsobel = cv.convertScaleAbs(xsobel)
+  ysobel = cv.convertScaleAbs(ysobel)
+  return cv.addWeighted(xsobel, .5, ysobel, .5, 0)
+
+def binarize(entrie: cv.Mat, llevel: uint8 = 40):
+  def binarize(entrie: uint8):
+    if entrie < llevel: return WHITE
+    return BLACK
+  output = entrie.copy()
+  for tupla in getTuplas(entrie.shape):
+    output[tupla] = binarize(output[tupla])
+  return output
