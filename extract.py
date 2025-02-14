c8abf2d3d0d5c0dc801a10e72feb593ed9bc84e0
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2025-02-13 23:44:39 -0300

    Update extract.py
    Agora o código inteiro em python foi comentado, deixando bem claro cada uma das funções

--- extract.py
+++ extract.py
@@ -16,223 +16,438 @@ NDArray = NDArray[integer[Any] | floating[Any]]
 BLACK, WHITE, RED = 0, 255, (0, 0, 255)
 NDArrays = list[NDArray]
 
-def bgr2gray(entrie: MatLike):
-  return cvtColor(entrie, COLOR_BGR2GRAY)
+def bgr2gray(image: MatLike) -> MatLike:
+  """
+  Converte uma imagem de BGR para escala de cinza.
+  
+  Args:
+    image (MatLike): Imagem em formato BGR.
+  
+  Returns:
+    MatLike: Imagem em escala de cinza.
+  """
+  return cvtColor(image, COLOR_BGR2GRAY)
 
-def gray2bgr(entrie: MatLike):
-  return cvtColor(entrie, COLOR_GRAY2BGR)
+def gray2bgr(image: MatLike) -> MatLike:
+  """
+  Converte uma imagem de escala de cinza para BGR.
+  
+  Args:
+    image (MatLike): Imagem em escala de cinza.
+  
+  Returns:
+    MatLike: Imagem em formato BGR.
+  """
+  return cvtColor(image, COLOR_GRAY2BGR)
 
-class imopen:
-  __slots__ = ("entrie", "index")
+class ImageOpener:
+  """
+  Classe para abrir imagens de um diretório ou arquivo.
+  """
+  __slots__ = ("path", "index")
 
-  def __init__(self, entrie: str):
-    if not isdir(entrie) and not isfile(entrie):
-      raise FileNotFoundError(f"{entrie!r} não existe!")
-    self.entrie, self.index = entrie, None
+  def __init__(self, path: str):
+    """
+    Inicializa a classe com o caminho do arquivo ou diretório.
+    
+    Args:
+      path (str): Caminho do arquivo ou diretório.
+    
+    Raises:
+      FileNotFoundError: Se o caminho não existir.
+    """
+    if not isdir(path) and not isfile(path):
+      raise FileNotFoundError(f"{path!r} não existe!")
+    self.path, self.index = path, None
 
   def __getitem__(self, index: int | slice):
+    """
+    Define o índice ou fatia para iteração.
+    
+    Args:
+      index (int | slice): Índice ou fatia.
+    
+    Returns:
+      self: Instância da classe.
+    """
     if isinstance(index, (int, slice)):
       self.index = index
     return self
 
   def __iter__(self):
-    def imopen_core(entrie: str):
-      name, extension = splitext(entrie)
-      valid = (".bmp", ".dib", ".jpeg", ".jpg", ".jpe",
-        ".jp2", ".png", ".pnm", ".avif", ".pbm", ".pgm",
-        ".ppm", ".pxm", ".webp", ".pfm", ".sr", ".ras",
-        ".tiff", ".tif", ".exr", ".hdr", ".pic")
-      if not extension in valid:
-        raise TypeError(f"{entrie!r} não é uma imagem!")
-      if (image := imread(entrie)) is None:
-        raise TypeError(f"{entrie!r} não pôde ser aberto!")
+    """
+    Itera sobre os arquivos de imagem no diretório ou arquivo.
+    
+    Yields:
+      tuple: Nome do arquivo e imagem em escala de cinza.
+    """
+    def open_image_core(path: str):
+      name, extension = splitext(path)
+      valid_extensions = (".bmp", ".dib", ".jpeg", ".jpg",
+        ".jpe", ".jp2", ".png", ".pnm", ".avif", ".pbm",
+        ".pgm", ".ppm", ".pxm", ".webp", ".pfm", ".sr",
+        ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic")
+      if extension not in valid_extensions:
+        raise TypeError(f"{path!r} não é uma imagem!")
+      if (image := imread(path)) is None:
+        raise TypeError(f"{path!r} não pôde ser aberto!")
       if isdir(name): rmtree(name)
       return name, bgr2gray(image)
 
-    entrie, index = self.entrie, self.index
-    if isfile(entrie):
-      yield imopen_core(entrie)
+    path, index = self.path, self.index
+    if isfile(path):
+      yield open_image_core(path)
       return
     if isinstance(index, int):
-      files = [e for e in scandir(entrie) if e.is_file()]
-      yield imopen_core(join(entrie, files[index].name))
+      files = [e for e in scandir(path) if e.is_file()]
+      yield open_image_core(join(path, files[index].name))
       return
     if isinstance(index, slice):
-      yield from (imopen_core(join(entrie, e.name)) for e in
-        [e for e in scandir(entrie) if e.is_file()][index])
+      iter(open_image_core(join(path, e.name)) for e in
+        [e for e in scandir(path) if e.is_file()][index])
       return
     if index is None:
-      yield from (imopen_core(join(entrie, e.name))
-        for e in scandir(entrie) if e.is_file())
+      iter(open_image_core(join(path, e.name))
+        for e in scandir(path) if e.is_file())
       return
 
-def cutImage(entrie: MatLike, shape: Size):
+def cut_image(image: MatLike, shape: Size) -> MatLike:
+  """
+  Corta a imagem em uma região específica e redimensiona.
+
+  Args:
+    image (MatLike): Imagem de entrada.
+    shape (Size): Tamanho desejado da imagem.
+
+  Raises:
+    TypeError: Se o shape não tiver tamanho 2.
+
+  Returns:
+    MatLike: Imagem cortada e redimensionada.
+  """
   if len(shape) != 2:
     raise TypeError("shape sem tamanho 2!")
   x1, x2, y1, y2 = 180, 2445, 305, 6095
-  cutted = entrie[x1:x2, y1:y2]
-  cutted = resize(cutted, shape[::-1])
-  print("cutImage terminado!")
-  return cutted
+  cropped = image[x1:x2, y1:y2]
+  resized = resize(cropped, shape[::-1])
+  print("cut_image terminado!")
+  return resized
 
-def gaussProcess(entrie: MatLike, size = 19, C = 10):
-  output = adaptiveThreshold(entrie, 255,
+def gauss_process(image: MatLike, size: int = 19, C: int = 10) -> MatLike:
+  """
+  Aplica processamento Gaussiano na imagem.
+
+  Args:
+    image (MatLike): Imagem de entrada.
+    size (int, optional): Tamanho do bloco. Padrão é 19.
+    C (int, optional): Constante subtraída da média. Padrão é 10.
+
+  Returns:
+    MatLike: Imagem processada.
+  """
+  output = adaptiveThreshold(image, 255,
     ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, size, C)
   output = GaussianBlur(output, (11, 11), 0)
-  print("gaussProcess terminado!")
+  print("gauss_process terminado!")
   return output
 
-def cannyFilter(entrie: MatLike):
-  output = Canny(entrie, 200, 255, L2gradient = True)
-  print("cannyFilter terminado!")
+def canny_filter(image: MatLike) -> MatLike:
+  """
+  Aplica filtro Canny na imagem.
+
+  Args:
+    image (MatLike): Imagem de entrada.
+
+  Returns:
+    MatLike: Imagem filtrada.
+  """
+  output = Canny(image, 200, 255, L2gradient=True)
+  print("canny_filter terminado!")
   return output
 
-def dbscanSeparation(entrie: MatLike, quant = 2000):
-  def dbscanSeparation_core():
-    dim1, dim2 = (entrie == WHITE).nonzero()
+def dbscan_separation(image: MatLike, min_points: int = 2000) -> NDArrays:
+  """
+  Separa clusters usando DBSCAN.
+
+  Args:
+    image (MatLike): Imagem de entrada.
+    min_points (int, optional): Número mínimo de pontos em um cluster. Padrão é 2000.
+
+  Returns:
+    NDArrays: Lista de clusters separados.
+  """
+  def dbscan_separation_core():
+    dim1, dim2 = (image == WHITE).nonzero()
     locs = zeros((len(dim1), 3), int)
     locs.T[0], locs.T[1] = dim2, dim1
     ani = Ellipsoid(((.3, 0, 0), (0, 2.6, 0), (0, 0, 0)))
     clusters = find_clusters(locs, ani, 20, 6, 1e-9, 1)
     for cluster in set(clusters):
-      dim2, dim1, a = locs[clusters == cluster].T
-      if len(dim2) < quant: continue
+      dim2, dim1, _ = locs[clusters == cluster].T
+      if len(dim2) < min_points: continue
       yield vstack((dim1, dim2)).T[dim2.argsort()]
 
-  output = list(dbscanSeparation_core())
-  print("dbscanSeparation terminado!")
+  output = list(dbscan_separation_core())
+  print("dbscan_separation terminado!")
   return output
 
-def curveAdjust(D: NDArray, I: NDArray, n: int):
+def curve_adjust(D: NDArray, I: NDArray, degree: int) -> NDArray:
+  """
+  Ajusta curvas usando um polinômio de grau n.
+
+  Args:
+    D (NDArray): Dados de entrada.
+    I (NDArray): Valores de saída.
+    degree (int): Grau do polinômio.
+
+  Returns:
+    NDArray: Curva ajustada.
+  """
   D, I = D.astype(float), I.astype(float)
-  with errstate(all = "raise"):
-    G = D.reshape(-1, 1) ** range(n)
+  with errstate(all="raise"):
+    G = D.reshape(-1, 1) ** range(degree)
     A, B = (G.T @ G), (G.T @ I)
-    L, U = zeros((n, n)), A
-    L.flat[::(n + 1)] = 1
-    for i, ii in pairwise(range(n)):
+    L, U = zeros((degree, degree)), A
+    L.flat[::(degree + 1)] = 1
+    for i, ii in pairwise(range(degree)):
       L[ii:, i] = U[ii:, i] / U[i, i]
       U[ii:] -= L[ii:, i, None] * U[i]
-    x, y = zeros(n), zeros(n)
-    for i in range(n):
+    x, y = zeros(degree), zeros(degree)
+    for i in range(degree):
       y[i] = B[i] - (L[i] @ y)
-    for i in reversed(range(n)):
+    for i in reversed(range(degree)):
       temp = y[i] - (U[i] @ x)
       x[i] = temp / U[i, i]
     return (G @ x)
 
-def lineSeparation(entrie: NDArrays):
-  MIN, MAX = 550, 6
-  curves: NDArrays = []
-  basels: NDArrays = []
-  for array in entrie:
-    (A, B) = array.T
-    choice = curves
-    line = curveAdjust(B, A, 2)
-    res = (abs(line - A) < MAX)
-    if (~res).sum() < MIN:
-      choice = basels
-      A, B = A[res], B[res]
-    array = vstack((A, B)).T
-    choice.append(array)
-  print("lineSeparation terminado!")
-  return curves, basels
+def line_separation(clusters: NDArrays) -> tuple[NDArrays, NDArrays]:
+  """
+  Separa linhas em curvas e bases.
 
-def saveLines(lines: NDArrays, dname: str, dtype: str, shape: Size):
-  if len(shape) != 2: raise TypeError("shape sem tamanho 2!")
-  pasta, preta = f"{dname}/lines", zeros(shape, "uint8")
-  if not isdir(pasta): mkdir(pasta)
+  Args:
+    clusters (NDArrays): Lista de clusters.
+
+  Returns:
+    tuple[NDArrays, NDArrays]: Curvas e bases separadas.
+  """
+  MIN_POINTS, MAX_DIFF = 550, 6
+  curves: NDArrays = []
+  baselines: NDArrays = []
+  for cluster in clusters:
+    (A, B) = cluster.T
+    choice = curves
+    line = curve_adjust(B, A, 2)
+    res = (abs(line - A) < MAX_DIFF)
+    if (~res).sum() < MIN_POINTS:
+      choice = baselines
+      A, B = A[res], B[res]
+    cluster = vstack((A, B)).T
+    choice.append(cluster)
+  print("line_separation terminado!")
+  return curves, baselines
+
+def save_lines(lines: NDArrays, directory: str, dtype: str, shape: Size):
+  """
+  Salva linhas em arquivos de imagem e texto.
+
+  Args:
+    lines (NDArrays): Lista de linhas.
+    directory (str): Diretório para salvar os arquivos.
+    dtype (str): Tipo de linha (curva ou base).
+    shape (Size): Tamanho da imagem.
+
+  Raises:
+    TypeError: Se o shape não tiver tamanho 2.
+  """
+  if len(shape) != 2:
+    raise TypeError("shape sem tamanho 2!")
+  lines_dir = f"{directory}/lines"
+  black_image = zeros(shape, "uint8")
+  if not isdir(lines_dir): mkdir(lines_dir)
   for i, subarray in enumerate(lines):
-    imagem, (dim1, dim2) = preta.copy(), subarray.T
-    imagem[dim1, dim2] = WHITE
-    imwrite(fr"{pasta}\\{dtype}{i}.png", imagem)
-    with open(fr"{pasta}\\{dtype}{i}.txt", "w") as output:
+    image, (dim1, dim2) = black_image.copy(), subarray.T
+    image[dim1, dim2] = WHITE
+    imwrite(fr"{lines_dir}\\{dtype}{i}.png", image)
+    with open(fr"{lines_dir}\\{dtype}{i}.txt", "w") as output:
       output.write(f"Tamanho da imagem: {shape}\n")
       output.writelines(f"{elem}\n" for elem in subarray)
-  print("saveLines terminado!")
+  print("save_lines terminado!")
 
-def sobrepor(entrie: MatLike, listas: NDArrays):
-  output = gray2bgr(entrie)
-  dim1, dim2 = zip(*(e for a in listas for e in a))
+def overlay_lines(image: MatLike, lines: NDArrays) -> MatLike:
+  """
+  Sobrepõe linhas em uma imagem.
+
+  Args:
+    image (MatLike): Imagem de entrada.
+    lines (NDArrays): Lista de linhas.
+
+  Returns:
+    MatLike: Imagem com linhas sobrepostas.
+  """
+  output = gray2bgr(image)
+  dim1, dim2 = zip(*(e for line in lines for e in line))
   output[dim1, dim2] = RED
-  print("sobrepor terminado!")
+  print("overlay_lines terminado!")
   return output
 
-def differs(curves: NDArrays, basels: NDArrays, dname: str, const: float):
-  if not isdir(pasta := f"{dname}/diffs"): mkdir(pasta)
-  ecurves, ebasels = enumerate(curves), enumerate(basels)
-  for (c, curve), (b, basel) in product(ecurves, ebasels):
-    mean = basel.T[0].mean(None, int)
+def calculate_differences(curves: NDArrays, baselines: NDArrays, directory: str, const: float):
+  """
+  Calcula diferenças entre curvas e bases.
+
+  Args:
+    curves (NDArrays): Lista de curvas.
+    baselines (NDArrays): Lista de bases.
+    directory (str): Diretório para salvar os arquivos.
+    const (float): Constante de conversão.
+  """
+  differences_dir = f"{directory}/diffs"
+  if not isdir(differences_dir): mkdir(differences_dir)
+  curves_and_baselines = product(enumerate(curves), enumerate(baselines))
+  for (curve_index, curve), (baseline_index, baseline) in curves_and_baselines:
+    mean = baseline.T[0].mean(None, int)
     values = (mean - curve.T[0]) * const
-    with open(fr"{pasta}\\curve{c}base{b}.txt", "w") as output:
-      output.writelines(f"({e1}, {mean}), {e2} - {value}\n"
-        for (e1, e2), value in zip(curve, values))
-  print("differs terminado!")
+    with open(fr"{differences_dir}\\curve{curve_index}base{baseline_index}.txt", "w") as output:
+      output.writelines(f"({e1}, {mean}), {e2} - {value}\n" for (e1, e2), value in zip(curve, values))
+  print("calculate_differences terminado!")
 
 def components_with_t(base: float, mm_const: float, mm_diff: NDArray,
-                      q_comp: float, t_base: float, t_diff: float):
+                      q_comp: float, t_base: float, t_diff: float) -> NDArray:
+  """
+  Calcula componentes com t.
+
+  Args:
+    base (float): Valor base.
+    mm_const (float): Constante de conversão.
+    mm_diff (NDArray): Diferença em milímetros.
+    q_comp (float): Componente q.
+    t_base (float): Valor base de t.
+    t_diff (float): Diferença de t.
+
+  Returns:
+    NDArray: Componentes calculados.
+  """
   return (base + (mm_const * mm_diff) - (q_comp * (t_base - t_diff)))
 
-def components_wout_t(base: float, mm_const: float, mm_diff: NDArray):
+def components_without_t(base: float, mm_const: float, mm_diff: NDArray) -> NDArray:
+  """
+  Calcula componentes sem t.
+
+  Args:
+    base (float): Valor base.
+    mm_const (float): Constante de conversão.
+    mm_diff (NDArray): Diferença em milímetros.
+
+  Returns:
+    NDArray: Componentes calculados.
+  """
   return components_with_t(base, mm_const, mm_diff, 0, 0, 0)
 
-d_component = t_component = components_wout_t
+d_component = t_component = components_without_t
 h_component = z_component = components_with_t
 
-def differs_new(curves: NDArrays, basels: NDArrays, px_to_mm: float):
-  (h, d, z) = curves
-  def differs_core():
-    for basel in basels:
-      meanbase = basel.T[0].mean().round().astype(int)
-      diff_h_mm = ((meanbase - h.T[0]) * px_to_mm)
-      diff_d_mm = ((meanbase - d.T[0]) * px_to_mm)
-      diff_z_mm = ((meanbase - z.T[0]) * px_to_mm)
+def calculate_new_differences(curves: NDArrays, baselines: NDArrays,
+    px_to_mm: float) -> tuple[NDArray, NDArray, NDArray]:
+  """
+  Calcula novas diferenças entre curvas e bases.
+
+  Args:
+    curves (NDArrays): Lista de curvas.
+    baselines (NDArrays): Lista de bases.
+    px_to_mm (float): Conversão de pixels para milímetros.
+
+  Returns:
+    tuple[NDArray, NDArray, NDArray]: Diferenças calculadas.
+  """
+  h_curve, d_curve, z_curve = curves
+  def calculate_core():
+    for baseline in baselines:
+      mean_base = baseline.T[0].mean().round().astype(int)
+      diff_h_mm = ((mean_base - h_curve.T[0]) * px_to_mm)
+      diff_d_mm = ((mean_base - d_curve.T[0]) * px_to_mm)
+      diff_z_mm = ((mean_base - z_curve.T[0]) * px_to_mm)
       yield (diff_h_mm, diff_d_mm, diff_z_mm)
-  ret = zip(*differs_core())
-  print("differs terminado!")
-  return ret
+  result = zip(*calculate_core())
+  print("calculate_new_differences terminado!")
+  return result
 
 Sequence = tuple[NDArray, ...]
-def h_comp_calc(diffs_h: Sequence, h_base: float, h_const: float, hq_comp: float, t_comp: float, dir: str):
-  for b, diff_h in enumerate(diffs_h):
-    with open(fr"{dir}/diffs/h{b}.txt", "w") as output:
+
+def calculate_h_components(diffs_h: Sequence, h_base: float, h_const: float,
+    hq_comp: float, t_comp: float, directory: str):
+  """
+  Calcula componentes h.
+
+  Args:
+    diffs_h (Sequence): Diferenças h.
+    h_base (float): Valor base de h.
+    h_const (float): Constante de h.
+    hq_comp (float): Componente q de h.
+    t_comp (float): Componente t.
+    directory (str): Diretório para salvar os arquivos.
+  """
+  for index, diff_h in enumerate(diffs_h):
+    with open(fr"{directory}/diffs/h{index}.txt", "w") as output:
       output.writelines(map(str, h_component(h_base, h_const, diff_h, hq_comp, t_comp)))
 
-def d_comp_calc(diffs_d: Sequence, d_base: float, d_const: float, dir: str):
-  for b, diff_d in enumerate(diffs_d):
-    with open(fr"{dir}/diffs/d{b}.txt", "w") as output:
-      output.writelines(map(str, d_component(d_base, d_const, diff_d)))
+def calculate_d_components(diffs_d: Sequence, d_base: float, d_const: float, directory: str):
+    """
+    Calcula componentes d.
 
-def z_comp_calc(diffs_z: Sequence, z_base: float, z_const: float, zq_comp: float, t_comp: float, dir: str):
-  for b, diff_z in enumerate(diffs_z):
-    with open(fr"{dir}/diffs/z{b}.txt", "w") as output:
+    Args:
+      diffs_d (Sequence): Diferenças d.
+      d_base (float): Valor base de d.
+      d_const (float): Constante de d.
+      directory (str): Diretório para salvar os arquivos.
+    """
+    for index, diff_d in enumerate(diffs_d):
+      with open(fr"{directory}/diffs/d{index}.txt", "w") as output:
+        output.writelines(map(str, d_component(d_base, d_const, diff_d)))
+
+def calculate_z_components(diffs_z: Sequence, z_base: float, z_const: float,
+      zq_comp: float, t_comp: float, directory: str):
+  """
+  Calcula componentes z.
+
+  Args:
+    diffs_z (Sequence): Diferenças z.
+    z_base (float): Valor base de z.
+    z_const (float): Constante de z.
+    zq_comp (float): Componente q de z.
+    t_comp (float): Componente t.
+    directory (str): Diretório para salvar os arquivos.
+  """
+  for index, diff_z in enumerate(diffs_z):
+    with open(fr"{directory}/diffs/z{index}.txt", "w") as output:
       output.writelines(map(str, z_component(z_base, z_const, diff_z, zq_comp, t_comp)))
 
 def main():
+  """
+  Função principal para processar as imagens.
+  """
   shape, const = (1800, 4590), (200 / 4590)
-  for name, opened in imopen("Imagens"):
+  for name, opened in ImageOpener("Imagens"):
     print(f"Imagem atual: {name}")
     if not isdir(name): mkdir(name)
 
-    cutted = cutImage(opened, shape)
-    imwrite(f"{name}/cutted.png", cutted)
-    gauss = gaussProcess(cutted)
-    imwrite(f"{name}/gauss.png", gauss)
-    canny = cannyFilter(gauss)
-    imwrite(f"{name}/canny.png", canny)
+    cropped_image = cut_image(opened, shape)
+    imwrite(f"{name}/cutted.png", cropped_image)
+    gauss_image = gauss_process(cropped_image)
+    imwrite(f"{name}/gauss.png", gauss_image)
+    canny_image = canny_filter(gauss_image)
+    imwrite(f"{name}/canny.png", canny_image)
 
-    skeleton = skeletonize(canny, 15)
-    imwrite(f"{name}/skeleton.png", skeleton)
+    skeleton_image = skeletonize(canny_image, 15)
+    imwrite(f"{name}/skeleton.png", skeleton_image)
 
-    separed = dbscanSeparation(skeleton)
-    curves, basels = lineSeparation(separed)
+    separated_clusters = dbscan_separation(skeleton_image)
+    curves, baselines = line_separation(separated_clusters)
 
-    saveLines(curves, name, "curve", shape)
-    saveLines(basels, name, "basel", shape)
-    differs(curves, basels, name, const)
+    save_lines(curves, name, "curve", shape)
+    save_lines(baselines, name, "basel", shape)
+    calculate_differences(curves, baselines, name, const)
 
-    dbscan1 = sobrepor(skeleton, curves)
-    dbscan2 = sobrepor(canny, curves)
+    dbscan1 = overlay_lines(skeleton_image, curves)
+    dbscan2 = overlay_lines(canny_image, curves)
     imwrite(f"{name}/dbscan1.png", dbscan1)
     imwrite(f"{name}/dbscan2.png", dbscan2)
     print("\n----------------------\n")
